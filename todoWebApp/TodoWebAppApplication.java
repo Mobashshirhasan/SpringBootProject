4ac38eb769cda9b2a07a3137dbebffceeb1e9979
Mobashshir Hasan <mobashshir12@gmail.com>
2024-10-24 11:00:54 +0530

    Update TodoWebAppApplication.java

--- todoWebApp/TodoWebAppApplication.java
+++ todoWebApp/TodoWebAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TodoWebAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TodoWebAppApplication.class, args);
+		SpringApplication.run(TodoWebAppApplication.class, args)
 	}
 
 }
